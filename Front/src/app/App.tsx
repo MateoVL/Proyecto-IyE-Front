7f144c255d92500973626d8440500ff5d6427b06
Fran <francisca.cavieres@usach.cl>
2026-05-19 18:13:03 -0400

    Adding Home Viwew and navigation from it

--- Front/src/app/App.tsx
+++ Front/src/app/App.tsx
@@ -5,17 +5,23 @@ import { Footer } from './components/Footer';
 
 import { NurseDashboard } from './components/NurseDashboard';
 import { DirectorDashboard } from './components/DirectorDashboard';
-
+import { HomeView } from './components/HomeView';
 import { ScheduleAppointment } from './components/ScheduleAppointment';
 
 export default function App() {
+  function handleSelectRole(role: 'director' | 'nurse'): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navbar />
 
       <main className="flex-1">
         <Routes>
-          <Route path="/" element={<NurseDashboard />} />
+
+          <Route path="/" element={<HomeView onSelectRole={handleSelectRole} />} />
+          <Route path="/nurse" element={<NurseDashboard />} />
 
           <Route
             path="/director"
